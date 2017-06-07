9759f170b5133ae07fbab7c469edc201f4a23fb7
matteson <andrew.matteson@appliedbiomath.com>
2017-06-07 19:00:29 -0400

    Add express server

---
+++ server.js
@@ -0,0 +1,35 @@
+/**
+ * Created by andrew on 6/7/17.
+ */
+
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+
+
+var app = express();
+
+var staticRoot = __dirname + '/dist/';
+app.set('port', (process.env.PORT || 61337));
+app.use(express.static(staticRoot));
+
+app.use(function(req, res, next){
+  // if the request is not html then move along
+  var accept = req.accepts('html', 'json', 'xml');
+  if(accept !== 'html'){
+    return next();
+  }
+
+  // if the request has a '.' assume that it's for a file, move along
+  var ext = path.extname(req.path);
+  if (ext !== ''){
+    return next();
+  }
+  fs.createReadStream(staticRoot + '/index.html').pipe(res);
+});
+
+app.listen(app.get('port'), function() {
+  console.log('app running on port', app.get('port'));
+});
+
+
